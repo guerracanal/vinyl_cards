c5fb358665537a16eb4232891e3152c3cb0ee33c
Jorge Guerra Canal <jorge.guerra@bluetab.net>
2023-04-20 00:29:00 +0200

    add black border inside color border

--- utils/card_generator.py
+++ utils/card_generator.py
@@ -43,7 +43,8 @@ def generator(album, resolution, icon):
     print('y_position: ' + str(y_position))
     
     text_box_position = 4000
-    add_horizontal_line(album_art.shape[0], text_box_position - 4*spacing, album_art)
+    add_horizontal_line(album_art.shape[0] + 100, text_box_position - 4*spacing, album_art)
+    #add_horizontal_black_lines(album_art.shape[0] + 100, text_box_position - 4*spacing)
 
     y_position = text_box_position
 
@@ -61,21 +62,23 @@ def generator(album, resolution, icon):
     y_position += 2*spacing
     
     text =  data['release_date'] + ' - ' + str(data['total_tracks']) + ' tracks  (' +  data['playtime'] + ')'
-    add_details_to_card(text, resolution, y_position, spacing)
+    add_details_to_card(text, resolution, y_position + 10, spacing)
     
     # update y position for next element
     y_position += spacing
     
+    #logo = add_spotify_logo(card, resolution, spacing)
+    add_spotify_code(album, resolution, spacing)
+
     # add border to card with colors of album art
     add_border_to_card(album_art)
+    add_black_border_to_card(False)
     
     # update y position for next element
     y_position += 1*spacing
     
     #add_tracks_album_to_card(card, resolution, y_position, spacing)
-    #logo = add_spotify_logo(card, resolution, spacing)
-    spotify_code = add_spotify_code(album, resolution, spacing)
-    
+        
     #add_label(data['record'] + ' - ' + data['album_type'] + ' ', (spacing, resolution[0]-163))
     #add_label(data['release_date'] + ' (' + data['copyright'] + ')' , (spacing, resolution[0]-spacing))
     
@@ -150,10 +153,10 @@ def add_album_art_to_card(album_art, resolution, spacing):
     # set image in card
     card[y_offset:y_offset+album_art.shape[0], x_offset:x_offset+album_art.shape[1]] = album_art
 
-def get_font_scale(text, resolution, spacing, font_scale_factor, thickness):
+def get_font_scale(text, resolution, spacing, font_scale_factor, thickness, margin):
     
     for i in range(2*spacing, 50, -5):
-        i = i/100
+        i = i/margin
         textsize = cv2.getTextSize(text, cv2.FONT_HERSHEY_COMPLEX, i*font_scale_factor, thickness)
         if textsize[0][0] <= (resolution[1]-2*spacing):
             font_scale = i*font_scale_factor
@@ -165,7 +168,7 @@ def add_title_to_card(text, resolution, y_position, spacing):
     font_scale = 0
     font_scale_factor = 5
     thickness = 15
-    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness)
+    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness, 120)
 
     print('title:' + str(font_scale))    
     # Calculate x position to center text horizontally
@@ -180,7 +183,7 @@ def add_subtitle_to_card(text, resolution, y_position, spacing):
     font_scale_factor = 3
     thickness = 10
 
-    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness)
+    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness, 110)
 
     print('subtitle:' + str(font_scale))
     # Calculate x position to center text horizontally
@@ -195,7 +198,7 @@ def add_details_to_card(text, resolution, y_position, spacing):
     font_scale_factor = 5
     thickness = 2
 
-    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness)
+    font_scale, textsize = get_font_scale(text, resolution, spacing, font_scale_factor, thickness, 200)
 
     print('details:' + str(font_scale))
 
@@ -221,13 +224,24 @@ def add_horizontal_line(y_start, y_end, album_art):
         x_end = x_start + section_width
         cv2.rectangle(horizontal_line, (x_start, 0), (x_end, line_height), palette[i], -1)
     
-    alpha = 0.3  # define alpha value between 0 (fully transparent) and 1 (fully opaque)
+    alpha = 0.5  # define alpha value between 0 (fully transparent) and 1 (fully opaque)
     beta = 1 - alpha  # calculate beta value
 
     # modify opacity of the rectangle
     horizontal_line = cv2.addWeighted(horizontal_line, alpha, np.ones(horizontal_line.shape, dtype=np.uint8) * 255, beta, 0, horizontal_line)
 
     card[y_start:y_end, :] = horizontal_line
+
+def add_horizontal_black_lines(y_start, y_end):
+
+    # add black lines above and below the colored line
+    black_line_height = 5
+    black_line_top = y_start - black_line_height
+    black_line_bottom = y_end #+ black_line_height
+    black_line = np.zeros((black_line_height, card.shape[1], 3), np.uint8)
+    black_line[:] = (0, 0, 0)
+    card[black_line_top:black_line_top+black_line_height, :] = black_line
+    card[black_line_bottom:black_line_bottom+black_line_height, :] = black_line
 
 
 def add_border_to_card(album_art):
@@ -274,6 +288,46 @@ def add_border_to_card(album_art):
     card[0:border_width, :] = top_border
     card[card.shape[0]-border_width:card.shape[0], :] = bottom_border
 
+def add_black_border_to_card(round_corners=False):
+    global card
+
+    border_width = 100
+    thickness = 20
+
+    # add black border inside existing border
+    inner_border_width = border_width // 2
+    inner_border_height = card.shape[0] - 2 * inner_border_width
+    
+    inner_border = np.zeros((inner_border_height, card.shape[1] - 2 * inner_border_width, 3), np.uint8)
+    inner_border[:] = (0, 0, 0)
+    
+    if round_corners:
+        # Set the corner radius
+        corner_radius = 50
+
+        # Draw the rectangle with rounded corners
+        rectangle_color = (0, 0, 0)
+        
+        # corners
+        top_left = (border_width, border_width)
+        bottom_left = (border_width, card.shape[0] - border_width)
+        top_right = (card.shape[1] - border_width, border_width)
+        bottom_right = (card.shape[1] - border_width, card.shape[0] - border_width)
+
+        cv2.rectangle(card, (border_width, border_width), (card.shape[1] - border_width, card.shape[0] - border_width), (0, 0, 0), thickness)
+        
+        cv2.ellipse(card, (top_left[0] + corner_radius, top_left[1] + corner_radius), (corner_radius, corner_radius), 0, 180, 270, rectangle_color, thickness)
+        #cv2.ellipse(card, (top_left[0] + corner_radius - 20 , top_left[1] + corner_radius - 20), (corner_radius - 20, corner_radius - 20), 0, 180, 270, rectangle_color, thickness)
+
+        cv2.ellipse(card, (top_right[0] - corner_radius, top_right[1] + corner_radius), (corner_radius, corner_radius), 270, 0, 90, rectangle_color, thickness)
+        
+        cv2.ellipse(card, (bottom_left[0] + corner_radius, bottom_left[1] - corner_radius), (corner_radius, corner_radius), 0, 90, 180, rectangle_color, thickness)
+        
+        cv2.ellipse(card, (bottom_right[0] - corner_radius, bottom_right[1] - corner_radius), (corner_radius, corner_radius), 0, 0, 90, rectangle_color, thickness)
+
+    else:
+        cv2.rectangle(card, (border_width, border_width), (card.shape[1] - border_width, card.shape[0] - border_width), (0, 0, 0), thickness)
+
 def add_spotify_code(album, resolution, spacing):
     global card  # declare card as a global variable
 
@@ -295,13 +349,10 @@ def add_spotify_code(album, resolution, spacing):
     new_height = int(height * scale)
 
     creditslogo = cv2.resize(creditslogo, (new_width, new_height), fx=scale, fy=scale)
-    #logo_x = resolution[1] - spacing - creditslogo.shape[1] # left position
     logo_x = resolution[1] - spacing - creditslogo.shape[1] - int((resolution[1] - 2*spacing - creditslogo.shape[1])/2) # center position
 
     card[resolution[0]-spacing-creditslogo.shape[0]:resolution[0]-spacing, logo_x:logo_x+creditslogo.shape[1]] = creditslogo
-    
-    return logo_x
-    
+        
 def add_icon(image_file, right_logo, resolution, spacing):
     global card  # declare card as a global variable
 
